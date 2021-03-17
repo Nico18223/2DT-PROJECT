20066c384e8f085b054eb64e393bf6913028cdb0
Vikki Sonneman <18223@nayland.school.nz>
2021-03-17 23:42:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+import time
+#making the letters print individualy, (simulated typing)
+def slowprint(string):
+	for letter in string + '\n':
+    #If I used print here, it would print on different lines, idk
+		sys.stdout.write(letter)
+    #makes the program output each letter at a time, instead of all at once
+		sys.stdout.flush()
+    #The time between each letter being printed
+		time.sleep(0.4/10)
+
+slowprint("Yay, it works")
+
