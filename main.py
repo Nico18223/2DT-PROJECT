94b5b25c90f8697b48d6fba6df2ac573024a541b
Nico Sonneman <18223@nayland.school.nz>
2021-03-21 22:21:48 +0000

    Practice countdown func using threading

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import time
+from threading import Timer
 #making the letters print individualy, (simulated typing)
 def slowprint(string):
 	for letter in string + '\n':
@@ -9,6 +10,19 @@ def slowprint(string):
 		sys.stdout.flush()
     #The time between each letter being printed
 		time.sleep(0.4/10)
-#comment
 #slowprint("Yay, it works")
-#Testing
+
+#fpoint stands for friendship_points
+f_point = 0
+
+
+#def countdown():
+    #print("Game over")
+
+# duration is in seconds
+#t = Timer(1 * 10, countdown)
+#t.start()
+
+# wait for time completion
+#t.join()
+
